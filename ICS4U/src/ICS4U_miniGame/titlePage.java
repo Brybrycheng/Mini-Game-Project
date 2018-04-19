10a4181fadef70c8f6653608dd364f047f02a28b
samsonchenson <38539327+samsonchenson@users.noreply.github.com>
2018-04-19 14:56:55 -0400

    Add files via upload

--- ICS4U/src/ICS4U_miniGame/titlePage.java
+++ ICS4U/src/ICS4U_miniGame/titlePage.java
@@ -42,7 +42,7 @@ public class titlePage extends JFrame {
 	 * Create the frame.
 	 */
 	public titlePage() {
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 566, 457);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
